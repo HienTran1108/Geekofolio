8f2c0f851e75d06deb52d3029188c28ee158debe
nguyendanghung97 <nguyendanghung97@hotmail.com>
2024-03-05 18:01:54 +0700

    function handle bart

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,3 @@ import { swiper } from './swiper'
 swiper()
 import { mousewheelSwiper } from './mousewheelswiper'
 mousewheelSwiper()
-
-
-// handlebars
-const temp = document.getElementById("template-1");
-console.log('temp',temp)
-if (temp) {
-  const template = Handlebars.compile(temp.innerHTML);
-  const data = {
-    "datass": JSON.stringify({ "name": "Alan", "hometown": "Somewhere, TX", "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]}), 
-    "name": "Alan", 
-    "hometown": "Somewhere, TX",
-    "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}] };
-  document.getElementById('text')!.innerHTML = template(data);
-}
