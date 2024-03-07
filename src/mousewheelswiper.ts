df6e6e6d73de365ad443c57c8ca86a5cd23f974b
nguyendanghung97 <nguyendanghung97@hotmail.com>
2024-03-07 22:04:01 +0700

    apply wowjs

--- src/mousewheelswiper.ts
+++ src/mousewheelswiper.ts
@@ -2,7 +2,7 @@ import { animationSlide } from "./gsap";
 export const mousewheelSwiper = new Swiper(".mousewheelSwiper", {
     direction: "horizontal",
     slidesPerView: 1,
-    spaceBetween: 90,
+    spaceBetween: 0,
     mousewheel: true,
     on: {
         init: (el: any) => animationSlide(el.slides[el.activeIndex], 0),
